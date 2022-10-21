2dd5b0ea5b6335a661c9fbbe4714956d34e9243b
jgjgill <79239852+jgjgill@users.noreply.github.com>
2022-10-21 11:00:50 +0900

    feat: Icon 관련 SVG 설정

--- src/pages/layout/Layout.tsx
+++ src/pages/layout/Layout.tsx
@@ -1,10 +1,13 @@
+import { GroupIcon, LogoutIcon, ProfileIcon } from '@assets/svgs';
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
     <div>
       <header>header</header>
-
+      <ProfileIcon />
+      <GroupIcon />
+      <LogoutIcon />
       <main>
         <Outlet />
       </main>
